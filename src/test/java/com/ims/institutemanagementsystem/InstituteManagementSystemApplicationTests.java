0dbf2d1d916c4f62d4a13e177edad4898e8e96be
Mazhar Baig <mazharbaig07@gmail.com>
2023-06-30 14:24:55 +0530

    IMS-1-Spring boot intial application

---
+++ src/test/java/com/ims/institutemanagementsystem/InstituteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.institutemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
